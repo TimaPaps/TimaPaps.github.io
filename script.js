e48516e931397672b95bf611bfb3ca597d25616e
TimaPaps <ptv.devstudio@gmail.com>
2020-04-23 01:37:53 +0500

    added social media links, moved pictures to a folder

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-$('.user-foto').click(function(){
-    $('.portfolio').css('background-color', 'rgb(255, 166, 0)')
-    $('.portfolio-left').css('background-color', 'rgb(216, 41, 41');
+$('.user-foto').click(function() {
+    var transTime = 5000;
+    $('.portfolio').css('background-color', 'rgb(255, 166, 0)');
+    $('.portfolio-left').css('background-color', 'rgb(216, 41, 41)');
     $('.portfolio-right').css('background-color', 'rgb(255, 166, 0)');
     $('h1').css('color', 'rgb(0, 47, 255)');    
     $('.portfolio-left p').fadeIn(3000);
 });
 
-$('.botton').click(function(){
+$('.botton').click(function() {
     $('.about-me').slideToggle(2500);
-    $('.about-me h3').css('color', 'rgb(0, 47, 255)')
-    $('.about-me span').css('color', 'rgb(30, 71, 252)')
+    $('.about-me h3').css('color', 'rgb(0, 47, 255)');
+    $('.about-me span').css('color', 'rgb(30, 71, 252)');
 });
